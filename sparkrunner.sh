23c0f72f3d09068e3acdbb713b8b97b01431e67f
Uri Rosenberg <urirosenberg@users.noreply.github.com>
2015-07-22 14:37:08 +0300

    added spark runner to launch

---
+++ sparkrunner.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export JAVA_HOME=/usr/java/default/jre/
+export SPARK_HOME=/opt/cloudera/parcels/CDH-5.3.1-1.cdh5.3.1.p0.5/lib/spark
+export PYTHONPATH=/usr/bin/python
+spark-submit --master spark://n12.mta.ac.il:7077  distrntn/distrntn2.py
+#spark-submit --master spark://n12.mta.ac.il:7077  --total-executor-cores 1 distrntn/distrntn.py
