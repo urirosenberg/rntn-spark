c3e20e1c74473e27c23949b41803fbf133aa0d53
Uri Rosenberg <urirosenberg@users.noreply.github.com>
2015-07-22 18:41:36 +0300

    Update sparkrunner.sh

--- sparkrunner.sh
+++ sparkrunner.sh
@@ -2,5 +2,5 @@
 export JAVA_HOME=/usr/java/default/jre/
 export SPARK_HOME=/opt/cloudera/parcels/CDH-5.3.1-1.cdh5.3.1.p0.5/lib/spark
 export PYTHONPATH=/usr/bin/python
-spark-submit --master spark://n12.mta.ac.il:7077  distrntn/distrntn.py
-#spark-submit --master spark://n12.mta.ac.il:7077  --total-executor-cores 1 distrntn/distrntn.py
+spark-submit --master spark://<master-url>:<master-port>  distrntn/distrntn.py
+#spark-submit --master spark://<master-url>:<master-port>  --total-executor-cores 1 distrntn/distrntn.py
